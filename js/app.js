bafa825d8b93028b1095e344e5d38d9a6ef3b47c
Miles D Goodings <miles@clevertech.biz>
2014-05-25 20:59:13 +1000

    code at point of demo

--- js/app.js
+++ js/app.js
@@ -100,31 +100,81 @@
 
       return svc;
     })
-    .controller('ZwipeCtrl', function($scope, AppService) {
-      $scope.offset = 0;
-      $scope.move = function(value) {
-        $scope.offset += value;
-        calculateAppPositions();
-      };
+    .controller('ZwipeCtrl', function($scope, $swipe, AppService) {
+      var stage = Sprite3D.stage(document.querySelector("#scroller")),
+        startX;
 
-      var stage = Sprite3D.stage(document.querySelector("#container"));
+      $scope.bearing = 0;
+      $scope.selected = 0;
+
+      $swipe.bind(ng.element(stage), {
+        'start': function(coords) {
+          startX = coords.x;
+        },
+        'move': function(coords) {
+          var delta = (coords.x - startX) / 16;
+
+          $scope.bearing += delta;
+          $scope.bearing = normalize($scope.bearing);
+
+          calculateAppPositions();
+        },
+        'end': function(coords) {
+          var length = $scope.apps.length,
+            theta = (360 / length);
+
+          $scope.selected = (Math.round($scope.bearing / theta) % length);
+          $scope.bearing = $scope.selected * theta;
+
+          calculateAppPositions();
+        },
+        'cancel': function(coords) {}
+      });
+
+      function calculateMovement(r, a, t) {
+        return {
+          x: Math.sin(r + a),
+          y: (Math.sin(r + 3 * Math.PI / 2 + a) / 8) * t,
+          z: -(Math.cos(r + a) + 1) / 2,
+          scale: (Math.sin(r + Math.PI / 2 + a) / 2) + 0.5
+        };
+      }
+
+      function normalize(degrees) {
+        var inRange = degrees % 360.0;
+        return (inRange < 0) ? 360 + inRange : inRange;
+      }
 
       function calculateAppPositions() {
         var radius = 150;
 
         for (var i = 0, length = $scope.apps.length; i < length; i++) {
-          var app = $scope.apps[i],
-            theta = (360 / length),
-            angle = (((i + $scope.offset) % length) * theta),
-            radAngle = (angle / 180) * Math.PI,
-            positionX = Math.sin(radAngle) * radius,
-            positionY = -Math.cos(radAngle) * radius;
+          var app = $scope.apps[i];
+          var element = ng.element(app.sprite);
+          var bearing = i * (360 / length);
+          var angle = normalize((360 - bearing) + $scope.bearing);
+          var rad = (angle * Math.PI) / 180;
+
+          while (rad < 0) {
+            rad += (Math.PI * 2);
+          }
+
+          while (rad > (Math.PI * 2)) {
+            rad -= (Math.PI * 2);
+          }
+
+          var movement = calculateMovement(rad, 0, 0);
 
           app.sprite
-            .origin(positionX + 30, positionY + 30)
-            .rotationX(angle / 2)
-            .rotationY(angle)
+            .origin((movement.x * radius) + 30, (movement.z * radius) + 30, movement.y * radius)
+            .scale(movement.scale)
             .update();
+
+          if (i === $scope.selected) {
+            element.addClass('selected');
+          } else {
+            element.removeClass('selected');
+          }
         }
       }
 
